981102c5ecd2015f7dd63db34fec8892cfa44a98
Michael Reimann <michael.reimann@epfl.ch>
2019-01-09 17:17:16 +0100

    Now taking feed-forward vs feed-back projections into account for layer profiles (according to the harris et al data)

--- white_matter/wm_recipe/layer_profiles/layer_profile_mixer.py
+++ white_matter/wm_recipe/layer_profiles/layer_profile_mixer.py
@@ -105,6 +105,19 @@ class ProfileMixer(object):
         self.profiles_m = ModuleProfiles(self.cfg)
         self.modules = mpr.module_idx
         self.pw_strength = proj_strength
+        self._hierarchy = ['ACAd', 'AIv', 'TEa', 'ACAv', 'MOs', 'VISC', 'ORBvl',
+                          'SSp-un', 'VISa', 'VISpor', 'VISam', 'AUDpo', 'VISpm',
+                          'FRP', 'ORBl', 'PL', 'RSPagl', 'AId', 'ORBm', 'VISal',
+                          'VISrl', 'ILA', 'SSp-tr', 'RSPd', 'MOp', 'VISli', 'VISl',
+                          'RSPv', 'SSs', 'SSp-bfd', 'VISpl', 'SSp-m', 'SSp-ul', 'AIp',
+                          'AUDd', 'SSp-ll', 'SSp-n', 'AUDp', 'VISp'] #Harris et al, Fig8e
+        self._m_hierarchy = ['prefrontal', 'anterolateral', 'medial', 'visual',
+                             'temporal', 'somatomotor']
+        '''No hierarchy reported for 4 regions. We put them in the middle (index 20).'''
+        self.idx2hierarchy = [self._hierarchy.index(_x) if _x in self._hierarchy
+                              else 20 for _x in mpr.region_names]
+        self.m_idx2hierarchy = [self._m_hierarchy.index(_x)
+                                for _x in mpr.module_names]
 
     @staticmethod
     def _treat_path(fn):
@@ -123,40 +136,60 @@ class ProfileMixer(object):
         kk = mpr.source_names
         idx_fr = mpr.module2idx(mod_fr)
         idx_to = mpr.module2idx(mod_to)
-        N = [(self.pw_strength(src_type=_k)[:, idx_to][idx_fr] > 0).sum()
-             for _k in kk]
+        pw_exists = [(self.pw_strength(src_type=_k)[:, idx_to][idx_fr] > 0)
+                     for _k in kk]
+        N = map(numpy.sum, pw_exists)
         N = numpy.array(N).astype(float) / numpy.sum(N)
-        return numpy.vstack([n * self.profiles_s.patterns[_k] for _k, n in zip(kk, N)]).sum(axis=0)
+        result = numpy.vstack([n * self.profiles_s.patterns[_k]
+                               for _k, n in zip(kk, N)]).sum(axis=0)
+        return result
+
+
+    def projection_is_ff(self, i, j):
+        m_i = mpr.idx2module(i)
+        m_j = mpr.idx2module(j)
+        if m_i == m_j or True:
+            return self.idx2hierarchy[i] > self.idx2hierarchy[j]
+        else:
+            return self._m_hierarchy.index(m_i) > self._m_hierarchy.index(m_j)
 
     def mix_module(self, source, mod_fr, mod_to):
         if mod_fr == mod_to:
             mod = mod_fr + '_intra'
         else:
             mod = mod_fr + '_inter'
+        #return self.profiles_m.patterns[mod] * self.profiles_s.patterns[source]
         adjust = self.profiles_m.patterns[mod] / self.predict_mix_from_sources(mod_fr, mod_to)
         return self.profiles_s.patterns[source] * adjust
 
-    def mix(self, source, i, j):
+    def mix(self, source, i, j, use_hierarchy=True):
         mod_fr = mpr.idx2module(i)
         mod_to = mpr.idx2module(j)
-        return self.mix_module(source, mod_fr, mod_to)
+        result = self.mix_module(source, mod_fr, mod_to)
+        if use_hierarchy:
+            if self.projection_is_ff(i, j):
+                result[[1, 3, 5]] *= 0.5
+            else:
+                result[[0, 2, 4]] *= 0.5
+        return result
 
-    def max(self, source, i, j):
-        rel = self.mix(source, i, j)
+    def max(self, source, i, j, **kwargs):
+        rel = self.mix(source, i, j, **kwargs)
         return numpy.argmax(rel)
 
     def max_module(self, source, mod_fr, mod_to):
         rel = self.mix_module(source, mod_fr, mod_to)
         return numpy.argmax(rel)
 
-    def full_mat(self, source):
+    def full_mat(self, source, **kwargs):
         N = mpr.n_regions()
-        return numpy.array([[self.max(source, i, j) for j in range(N)] for i in range(N)])
+        return numpy.array([[self.max(source, i, j, **kwargs)
+                             for j in range(N)] for i in range(N)])
 
     def compare_for_source(self, source):
         from matplotlib import pyplot as plt
         M = self.full_mat(source)
-        H = numpy.histogram((M + 1) * (self.pw_strength(src_type=source) > 0),
+        H = numpy.histogram(M + 1, weights=(self.pw_strength(src_type=source) > 0).astype(float),
                             bins=range(1, 8))[0]
         H = 6 * H.astype(float) / H.sum()
         plt.figure()
@@ -167,18 +200,22 @@ class ProfileMixer(object):
 
     def compare_for_module(self, module, inter):
         from matplotlib import pyplot as plt
-        M = numpy.dstack([(self.full_mat(_s) + 1) * (self.pw_strength(src_type=_s) > 0)
+        M = numpy.dstack([self.full_mat(_s) + 1
                           for _s in self.profiles_s.patterns.keys()])
+        W = numpy.dstack([self.pw_strength(src_type=_s) > 0
+                          for _s in self.profiles_s.patterns.keys()]).astype(float)
         M = M[self.modules[module][0]:self.modules[module][1], :, :]
+        W = W[self.modules[module][0]:self.modules[module][1], :, :]
         if inter:
             suffix = '_inter'
             N = numpy.max(numpy.hstack([self.modules.values()]))
             idxx = numpy.setdiff1d(range(N), range(self.modules[module][0], self.modules[module][1]))
-            M = M[:, idxx, :]
+            M = M[:, idxx, :]; W = W[:, idxx, :]
         else:
             suffix = '_intra'
             M = M[:, self.modules[module][0]:self.modules[module][1], :]
-        H = numpy.histogram(M, bins=range(1, 8))[0]
+            W = W[:, self.modules[module][0]:self.modules[module][1], :]
+        H = numpy.histogram(M, weights=W, bins=range(1, 8))[0]
         H = 6 * H.astype(float) / H.sum()
         plt.figure()
         plt.plot(range(1, 7), H, label='model')
